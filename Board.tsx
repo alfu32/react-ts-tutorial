778881133c70dca64bc6aa0496ad87cda31eef94
alfu32 <ioan.alferaru@gmail.com>
2021-01-20 12:41:00 +0100

    1 file modified

--- Board.tsx
+++ Board.tsx
@@ -2,12 +2,28 @@ import React from "react";
 
 export class Board extends React.Component {
   props = {
-    title: "default"
+    title: "default",
+    values: ["-", "-", "-", "-", "-", "-", "-", "-", "-"]
   };
   render() {
     return (
       <div className="board">
         <h4>{this.props.title}</h4>
+        <div className="row">
+          <Square value={this.props.values[0]} />
+          <Square value={this.props.values[1]} />
+          <Square value={this.props.values[2]} />
+        </div>
+        <div className="row">
+          <Square value={this.props.values[3]} />
+          <Square value={this.props.values[4]} />
+          <Square value={this.props.values[5]} />
+        </div>
+        <div className="row">
+          <Square value={this.props.values[6]} />
+          <Square value={this.props.values[7]} />
+          <Square value={this.props.values[8]} />
+        </div>
       </div>
     );
   }
