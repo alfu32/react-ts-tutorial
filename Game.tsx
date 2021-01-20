4ad391d549c40d20d647c3f82482667a6fe6fe1b
alfu32 <ioan.alferaru@gmail.com>
2021-01-20 12:33:01 +0100

    1 file modified

--- Game.tsx
+++ Game.tsx
@@ -1,13 +1,15 @@
 import React from "react";
+import { Board } from "./Board";
 
 export class Game extends React.Component {
-  props={
-    title:'default',
-  }
+  props = {
+    title: "default"
+  };
   render() {
     return (
       <div className="game">
         <h4>{this.props.title}</h4>
+        <Board title={"Board"} />
       </div>
     );
   }
