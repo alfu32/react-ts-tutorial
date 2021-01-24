ba57f485b58b70b50b33d4861d94c4615fbeb1f1
alfu32 <ioan.alferaru@gmail.com>
2021-01-24 09:26:39 +0100

    feature(redux): #5 : [WIP] boilerplate state

--- index.tsx
+++ index.tsx
@@ -5,33 +5,44 @@ import Hello from "./Hello";
 import "./style.css";
 
 // STORE -> globalized state
-import {createStore} from 'redux';
+import { createStore } from "redux";
 // ACTION descriptors ( intents ) for real actions {increment, decrement, etc.} -> an intention
 const increment = () => {
   return {
-    type: 'be.alf.tictactoe.INCREMENT',
-  };
-}
-const decrement = () => {
-  return {
-    type: 'be.alf.tictactoe.DECREMENT',
-  };
-}
-// REEDUCERS -> descries how actions are goingto transform current state into the next state
-const counter = (state = 0, action) => {
-  switch(action.type) {
-    case 'be.alf.tictactoe.INCREMENT': return state + 1;
-    case 'be.alf.tictactoe.DECREMENT': return state - 1;
-    default: return state;
+    type: "be.alf.tictactoe.INCREMENT"
   };
 };
-//  DISPATCH
+const decrement = () => {
+  return {
+    type: "be.alf.tictactoe.DECREMENT"
+  };
+};
+// REDUCERS -> descries how actions are goingto transform current state into the next state
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "be.alf.tictactoe.INCREMENT":
+      return state + 1;
+    case "be.alf.tictactoe.DECREMENT":
+      return state - 1;
+    default:
+      return state;
+  }
+};
+const store = createStore(counter);
+// Display it on the console
 
+store.subscribe(() => console.log(store.getState()));
+
+//  DISPATCH
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(decrement());
 
 interface AppProps {}
 interface AppState {
   name: string;
   games: Array<object>;
+  counter: number;
 }
 
 class App extends Component<AppProps, AppState> {
@@ -39,15 +50,23 @@ class App extends Component<AppProps, AppState> {
     super(props);
     this.state = {
       name: "React",
-      games: new Array()
+      games: new Array(),
+      counter: store.getState()
     };
+    store.subscribe(() => {
+      const newState = { ...this.state };
+      newState.counter = store.getState();
+      this.setState(newState);
+    });
   }
 
   render() {
     return (
       <div>
         <div className="counters">
-          <h4>Counters</h4>
+          <h4>Counters {this.state.counter}</h4>
+          <button onClick={() => store.dispatch(increment())}>+</button>
+          <button onClick={() => store.dispatch(decrement())}>-</button>
         </div>
         <Game
           title={"Game"}
