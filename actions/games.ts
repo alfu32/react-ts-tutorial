38e317f6ae57b9863bd850801b867649ce57d73a
alfu32 <ioan.alferaru@gmail.com>
2021-01-29 08:58:06 +0100

    feature(redux): #5: games actions and reducer

--- actions/games.ts
+++ actions/games.ts
@@ -1,14 +1,20 @@
 export const createCurrentGame = () => ({
-  type: "be.alf.tictactoe.game.CREATE",
+  type: "be.alf.tictactoe.game.CREATE"
 });
 export const storeCurrentGame = () => ({
-  type: "be.alf.tictactoe.game.STORE",
+  type: "be.alf.tictactoe.game.STORE"
+});
+export const restoreSavedGame = (index) => ({
+  type: "be.alf.tictactoe.game.LOAD",
+  payload:{
+    index,
+  }
 });
 export const move = (turn) => ({
   type: "be.alf.tictactoe.game.move.SET",
-  payload: {turn}
+  payload: { turn }
 });
-export const undo = (turn) => ({
+export const undo = (turn)  => ({
   type: "be.alf.tictactoe.game.move.BACK",
-  payload: {turn}
+  payload: { turn }
 });
--- reducers/games.ts
+++ reducers/games.ts
@@ -1,25 +1,35 @@
-export const gamesReducer = (state = null, action) => {
-  switch (action.type) {
-case "be.alf.tictactoe.game.CREATE":
+export const gamesReducer = (
+  state = {
+    games: [],
+    currentGame: { values: new Array(16).fill(null), turn: 0, history: [] }
+  },
+  action
+) => {
   state.games = state.games || [];
-  state.currentGame={
-    values: new Array(16).fill(null),
-    turn: 0,
-    history: []
-  }
-case "be.alf.tictactoe.game.STORE":
-  state.games = state.games || [];
-  state.games = state.games.concat([state.currentGame])
-case "be.alf.tictactoe.game.move.SET":
-  const nextCurrentGameState = {...state.currentGame};
+  let nextCurrentGameState = { ...state.currentGame };
   const currentGameValues = [...state.currentGame.values];
-  nextCurrentGameState.turn++;
-  nextCurrentGameState.history = nextCurrentGameState.history.concat(currentGameValues);
-  nextCurrentGameState.values[state.currentGame.turn] = state.currentGame.turn % 2 ? "X" : "0";
-  state.currentGames = {...nextCurrentGameState};
-  return state;
-case "be.alf.tictactoe.game.move.BACK":
-  state.games = state.games 
+  switch (action.type) {
+    case "be.alf.tictactoe.game.CREATE":
+      state.currentGame = {
+        values: new Array(16).fill(null),
+        turn: 0,
+        history: []
+      };
+    case "be.alf.tictactoe.game.STORE":
+      state.games = state.games.concat([state.currentGame]);
+    case "be.alf.tictactoe.game.LOAD":
+      state.games = state.games.concat([state.currentGame]);
+    case "be.alf.tictactoe.game.move.SET":
+      nextCurrentGameState.turn++;
+      nextCurrentGameState.history = nextCurrentGameState.history.concat(
+        currentGameValues
+      );
+      nextCurrentGameState.values[state.currentGame.turn] =
+        state.currentGame.turn % 2 ? "X" : "0";
+      state.currentGame = { ...nextCurrentGameState };
+      return state;
+    case "be.alf.tictactoe.game.move.BACK":
+
     default:
       return state;
   }
